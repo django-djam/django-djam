6309efc557243d438066628c5eb84e117523367f
Jason Kraus <jasonk@cukerinteractive.com>
2012-09-07 12:47:53 -0700

    stub out views

--- djam/riffs/models.py
+++ djam/riffs/models.py
@@ -4,17 +4,18 @@ from django.core.exceptions import ImproperlyConfigured
 from django import forms
 
 from djam.riffs.base import Riff
+from djam.views.models import ModelListView, ModelDetailView, ModelDeleteView, ModelHistoryView
 
 class ModelRiff(Riff):
     model = None
-    list_view = None
-    detail_view = None
-    delete_view = None
-    history_view = None
-    
+    list_view = ModelListView
+    detail_view = ModelDetailView
+    delete_view = ModelDeleteView
+    history_view = ModelHistoryView
+
     base_form_class = forms.ModelForm
     form_class = None
-    
+
     def __init__(self, *args, **kwargs):
         if not self.model:
             raise ImproperlyConfigured('Please specify a model')
@@ -23,37 +24,37 @@ class ModelRiff(Riff):
         if self.slug is None:
             self.slug = self.model._meta.module_name
         super(ModelRiff, self).__init__(*args, **kwargs)
-    
+
     def get_urls(self):
         urlpatterns = super(SiteRiff, self).get_urls()
-        
+
         def wrap(view):
             return self.as_view(view)
-        
+
         init = self.get_view_kwargs()
-        
+
         format_params = {
             'appname': self.model._meta.app_label,
             'modelname': self.slug
         }
-        
+
         urlpatterns += patterns('',
             url(r'^/$',
                 wrap(self.list_view.as_view(**init)),
-                name='{appname}_{modelname}_list'.format(**format_params),
+                name='{appname}_{modelname}_list'.format(**format_params)),
             url(r'^(?P<pk>\w+)/$',
-                wrap(self.detail_view.as_view(**init)),
-                name='{appname}_{modelname}_change').format(**format_params),
+                wrap(self.detail_view.as_view(**self.get_detail_view_kwargs())),
+                name='{appname}_{modelname}_change').format(**format_params)),
             url(r'^(?P<pk>\w+)/delete/$',
                 wrap(self.delete_view.as_view(**init)),
-                name='{appname}_{modelname}_delete').format(**format_params),
+                name='{appname}_{modelname}_delete').format(**format_params)),
             url(r'^(?P<pk>\w+)/history/$',
                 wrap(self.history_view.as_view(**init)),
-                name='{appname}_{modelname}_history').format(**format_params),
+                name='{appname}_{modelname}_history').format(**format_params)),
         )
-        
+
         return urlpatterns
-    
+
     def get_form_class(self):
         if self.form_class:
             return self.form_class
@@ -62,4 +63,13 @@ class ModelRiff(Riff):
                 model = self.model
         return GeneratedForm
 
+    def get_view_kwargs(self):
+        kwargs = self.get_view_kwargs()
+        kwargs['model'] = self.model
+        return kwargs
+
+    def get_detail_view_kwargs(self):
+        kwargs = self.get_view_kwargs()
+        kwargs['form_class'] = self.get_form_class()
+        return kwargs
 
