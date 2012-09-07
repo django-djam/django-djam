995b4ae8b4dd126ce36f0e5e050d39d0c90b482a
Stephen Burrows <stephen.r.burrows@gmail.com>
2012-09-07 17:34:41 -0400

    Forgot admin riff!

---
+++ djam/riffs/admin.py
@@ -0,0 +1,19 @@
+from django.conf.urls import patterns, include, url
+from django.contrib.sites.models import Site
+from django.utils.translation import ugettext_lazy as _
+
+from djam.riffs.base import Riff
+from djam.riffs.auth import AuthRiff
+
+
+class AdminRiff(Riff):
+    riff_classes = (AuthRiff,)
+    slug = 'admin'
+
+    def __init__(self, namespace=None):
+        site_name = Site.objects.get_current().name
+        self.verbose_name = _('Admin for {site_name}'.format(site_name=site_name))
+        super(AdminRiff, self).__init__(namespace=None)
+
+
+admin = AdminRiff()
