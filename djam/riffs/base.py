0b755f32c0cd2e5ce3ce15812536b957748fc854
dmpayton <derek.payton@gmail.com>
2012-09-07 15:49:29 -0400

    Add missing comma to riffs base.

--- djam/riffs/base.py
+++ djam/riffs/base.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, include
+from django.conf.urls.defaults import patterns, include, url
 from django.core.urlresolvers import reverse
 from django.core.exceptions import ImproperlyConfigured
 from django.http import HttpResponseRedirect
@@ -11,7 +11,7 @@ class Riff(object):
     riffs = []
     verbose_name = None
     slug = None
-    
+
     def __init__(self, name='djadmin', parent_riff=None):
         self.name = name
         self.parent_riff = parent_riff
@@ -23,40 +23,39 @@ class Riff(object):
         for riff_cls in self.riffs:
             riff = riff_cls(parent_riff=self)
             self._riffs.append(riff)
-    
+
     def get_urls(self):
         urlpatterns = self.get_extra_urls()
-        
+
         for riff in self._riffs:
-            urlpatterns += patterns(''
+            urlpatterns += patterns('',
                 url(r'^{slug}/'.format(slug=riff.slug),
                     include(riff.get_urls())),
             )
         return urlpatterns
-    
+
     def get_extra_urls(self):
         return patterns('',)
-    
+
     def urls(self):
         return self.get_urls(), None, self.name
     urls = property(urls)
-    
+
     def get_view_kwargs(self):
         return {'riff':self,}
-    
+
     def has_permission(self, request):
         if self.parent_riff:
             return self.parent_riff.has_permission(request)
         return True
-    
+
     def get_unauthorized_response(self, request):
         params = {'next':request.path}
         params = urlencode(params)
         return HttpResponseRedirect('{url}?{params}'.format(url=self.reverse('login'), params=params))
-    
+
     def as_view(self, view):
         return view
-    
+
     def reverse(self, name, *args, **kwargs):
         return reverse('{namespace}:{viewname}'.format(namespace=self.namespace, viewname=name), args=args, kwargs=kwargs)
-
