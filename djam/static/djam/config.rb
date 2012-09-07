b7efef9bc122917d39252c2048c9147efee935d9
Harris Lapiroff <harris@Harriss-MacBook.local>
2012-09-07 17:48:26 -0400

    Initial template and static files work.
    * Added Bootstrap and jQuery.
    * Created custom styles.
    * Created base templates and a login template.

---
+++ djam/static/djam/config.rb
@@ -0,0 +1,12 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+output_style = :compressed
+relative_assets = true
+line_comments = false
+preferred_syntax = :sass
--- example_djam_project/example_djam_project/urls.py
+++ example_djam_project/example_djam_project/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
 	url(r'^', include('djam.urls')),
+	url(r'^', TemplateView.as_view('login.html'))
 )
