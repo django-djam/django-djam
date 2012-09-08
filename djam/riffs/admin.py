6bcc6a47643d49efa96583e15aa605cba092ff63
Stephen Burrows <stephen.r.burrows@gmail.com>
2012-09-08 11:46:20 -0400

    Cleaned up auth urls.

--- djam/riffs/admin.py
+++ djam/riffs/admin.py
@@ -8,18 +8,26 @@ from djam.riffs.dashboard import DashboardRiff
 
 
 class AdminRiff(Riff):
-    riff_classes = (DashboardRiff, AuthRiff,)
+    auth_riff_class = AuthRiff
+    riff_classes = (DashboardRiff,)
     slug = 'admin'
 
     def __init__(self, namespace=None, app_name=None):
         site_name = Site.objects.get_current().name
-        self.verbose_name = _('Admin for {site_name}'.format(site_name=site_name))
+        if self.verbose_name is None:
+            self.verbose_name = _('Admin for {site_name}'.format(site_name=site_name))
         super(AdminRiff, self).__init__(parent=None,
                                         namespace=None,
                                         app_name=None)
+        self.auth_riff = self.auth_riff_class(parent=self)
 
     def get_default_url(self):
         return self.riffs[0].get_default_url()
+
+    def get_extra_urls(self):
+        return patterns('',
+            url(r'^', include(self.auth_riff.get_urls_tuple()))
+        )
 
 
 admin = AdminRiff(app_name='djam')
