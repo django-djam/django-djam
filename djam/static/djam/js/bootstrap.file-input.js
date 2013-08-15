d95f64abb8e422abf2c149181cd49d241ce9e4af
Stephen Burrows <stephen.r.burrows@gmail.com>
2013-08-15 12:26:45 -0700

    Corrected file input integration.

--- djam/static/djam/js/bootstrap.file-input.js
+++ djam/static/djam/js/bootstrap.file-input.js
@@ -33,7 +33,7 @@ $('input[type=file]').each(function(i,elem){
 
   // Now we're going to replace that input field with a Bootstrap button.
   // The input will actually still be there, it will just be float above and transparent (done with the CSS).
-  $elem.replaceWith('<a class="file-input-wrapper btn ' + $elem.attr('class') + '">'+buttonWord+input+'</a>');
+  $elem.replaceWith('<a class="file-input-wrapper btn btn-default ' + $elem.attr('class') + '">'+buttonWord+input+'</a>');
 })
 // After we have found all of the file inputs let's apply a listener for tracking the mouse movement.
 // This is important because the in order to give the illusion that this is a button in FF we actually need to move the button from the file input under the cursor. Ugh.
