5e6aced31e9fa8af499c9d5e2bc3d9802352b029
Stephen Burrows <stephen.r.burrows@gmail.com>
2013-09-19 19:20:25 -0700

    Corrected password change view to not fail spectacularly.

--- djam/batteries.py
+++ djam/batteries.py
@@ -1,33 +1,43 @@
 from django.contrib.auth.forms import AdminPasswordChangeForm
 from django.contrib.auth.models import User
 from django.conf.urls import patterns, url
+from django.http import Http404
 
 from djam.riffs.auth import AuthRiff
 from djam.riffs.dashboard import DashboardRiff
 from djam.riffs.models import ModelRiff
-from djam.views.generic import UpdateView
+from djam.views.generic import FormView
 from djam.views.models import ModelRiffMixin
 
 
-class UserPasswordView(ModelRiffMixin, UpdateView):
+class UserPasswordView(ModelRiffMixin, FormView):
     form_class = AdminPasswordChangeForm
     template_name = 'djam/models/auth/user/password-change.html'
+    model = User
 
     def get_success_url(self):
-        return self.riff.reverse('update', pk=self.object.pk)
+        return self.riff.reverse('update', pk=self.kwargs['pk'])
 
     def has_permission(self, request):
         return self.riff.has_change_permission(request)
 
     def get_form_kwargs(self):
         kwargs = super(UserPasswordView, self).get_form_kwargs()
-        kwargs['user'] = kwargs.pop('instance')
+        try:
+            self.object = User.objects.get(pk=self.kwargs['pk'])
+        except (User.DoesNotExist, KeyError):
+            raise Http404
+        kwargs['user'] = self.object
         return kwargs
+
+    def form_valid(self, form):
+        form.save()
+        return super(UserPasswordView, self).form_valid(form)
 
     def get_crumbs(self):
         crumbs = super(UserPasswordView, self).get_crumbs()
         crumbs += [
-            (self.riff.reverse('update', pk=self.object.pk),
+            (self.riff.reverse('update', pk=self.kwargs['pk']),
              unicode(self.object)),
             (None, 'Change password'),
         ]
@@ -38,7 +48,7 @@ class UserRiff(ModelRiff):
     model = User
 
     def get_password_change_kwargs(self):
-        return self.get_view_kwargs()
+        return {'riff': self}
 
     def get_extra_urls(self):
         urlpatterns = super(UserRiff, self).get_extra_urls()
